a845e77ba935ed629df36e9a72c251ed347f1b9b
brittanyyee <46659636+brittanyyee@users.noreply.github.com>
2020-09-17 14:35:41 -0400

    Update covid_callback.js

--- covid_callback.js
+++ covid_callback.js
@@ -42,7 +42,726 @@ covid_callback({
       },
       {
         "type": "Feature",
+        "properties": {eqfeed_callback({
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
         "properties": {
+          "location": "California",
+          "country_code": "us",
+          "latitude": 36.778261,
+          "longitude": -119.4179324,
+          "mag": 722035,
+          "dead": 13330,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Montana",
+          "country_code": "us",
+          "latitude": 46.8796822,
+          "longitude": -110.3625658,
+          "mag": 7715,
+          "dead": 109,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Colorado",
+          "country_code": "us",
+          "latitude": 39.5500507,
+          "longitude": -105.7820674,
+          "mag": 58392,
+          "dead": 1960,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "New Hampshire",
+          "country_code": "us",
+          "latitude": 43.1938516,
+          "longitude": -71.5723953,
+          "mag": 7309,
+          "dead": 432,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Illinois",
+          "country_code": "us",
+          "latitude": 40.6331249,
+          "longitude": -89.3985283,
+          "mag": 241282,
+          "dead": 8322,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "New York",
+          "country_code": "us",
+          "latitude": 40.7127753,
+          "longitude": -74.0059728,
+          "mag": 440947,
+          "dead": 32559,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Oregon",
+          "country_code": "us",
+          "latitude": 43.8041334,
+          "longitude": -120.5542012,
+          "mag": 27085,
+          "dead": 473,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Arkansas",
+          "country_code": "us",
+          "latitude": 35.20105,
+          "longitude": -91.8318334,
+          "mag": 62112,
+          "dead": 841,
+          "updated": "2020-09-03 18:45:37.999383+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Kentucky",
+          "country_code": "us",
+          "latitude": 37.8393332,
+          "longitude": -84.2700179,
+          "mag": 52702,
+          "dead": 1008,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Maine",
+          "country_code": "us",
+          "latitude": 45.253783,
+          "longitude": -69.4454689,
+          "mag": 4568,
+          "dead": 133,
+          "updated": "2020-09-03 18:45:37.999383+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "New Mexico",
+          "country_code": "us",
+          "latitude": 34.5199402,
+          "longitude": -105.8700901,
+          "mag": 25612,
+          "dead": 790,
+          "updated": "2020-09-03 18:45:37.999383+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Rhode Island",
+          "country_code": "us",
+          "latitude": 41.5800945,
+          "longitude": -71.4774291,
+          "mag": 22078,
+          "dead": 1051,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Vermont",
+          "country_code": "us",
+          "latitude": 44.5588028,
+          "longitude": -72.5778415,
+          "mag": 1637,
+          "dead": 58,
+          "updated": "2020-09-03 18:45:37.999383+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "West Virginia",
+          "country_code": "us",
+          "latitude": 38.5976262,
+          "longitude": -80.4549026,
+          "mag": 10658,
+          "dead": 233,
+          "updated": "2020-09-02 18:47:09.675065+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Wisconsin",
+          "country_code": "us",
+          "latitude": 43.7844397,
+          "longitude": -88.7878678,
+          "mag": 82261,
+          "dead": 1151,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Wyoming",
+          "country_code": "us",
+          "latitude": 43.0759678,
+          "longitude": -107.2902839,
+          "mag": 3911,
+          "dead": 41,
+          "updated": "2020-09-03 18:45:37.999383+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Minnesota",
+          "country_code": "us",
+          "latitude": 46.729553,
+          "longitude": -94.6858998,
+          "mag": 77137,
+          "dead": 1882,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Mississippi",
+          "country_code": "us",
+          "latitude": 32.3546679,
+          "longitude": -89.3985283,
+          "mag": 84365,
+          "dead": 2526,
+          "updated": "2020-09-03 18:45:37.999383+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Nebraska",
+          "country_code": "us",
+          "latitude": 41.4925374,
+          "longitude": -99.9018131,
+          "mag": 34995,
+          "dead": 408,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Guam",
+          "country_code": "us",
+          "latitude": 13.4769474,
+          "longitude": 144.7425932,
+          "mag": 2463,
+          "dead": 14,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "New Jersey",
+          "country_code": "us",
+          "latitude": 40.0583238,
+          "longitude": -74.4056612,
+          "mag": 194378,
+          "dead": 15964,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Northern Mariana Islands",
+          "country_code": "us",
+          "latitude": 15.0979,
+          "longitude": 145.6739,
+          "mag": 57,
+          "dead": 2,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "South Dakota",
+          "country_code": "us",
+          "latitude": 43.9695148,
+          "longitude": -99.9018131,
+          "mag": 14003,
+          "dead": 169,
+          "updated": "2020-09-03 18:45:37.999383+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Puerto Rico",
+          "country_code": "us",
+          "latitude": 18.4238478,
+          "longitude": -66.0648787,
+          "mag": 34003,
+          "dead": 443,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Iowa",
+          "country_code": "us",
+          "latitude": 41.8780025,
+          "longitude": -93.097702,
+          "mag": 66463,
+          "dead": 1134,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Virgin Islands",
+          "country_code": "us",
+          "latitude": 18.335765,
+          "longitude": -64.896335,
+          "mag": 1144,
+          "dead": 15,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Missouri",
+          "country_code": "us",
+          "latitude": 37.9642529,
+          "longitude": -91.8318334,
+          "mag": 88869,
+          "dead": 1669,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Alaska",
+          "country_code": "us",
+          "latitude": 64.2008413,
+          "longitude": -149.4936733,
+          "mag": 6181,
+          "dead": 36,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Tennessee",
+          "country_code": "us",
+          "latitude": 35.5174913,
+          "longitude": -86.5804473,
+          "mag": 157831,
+          "dead": 1761,
+          "updated": "2020-09-03 18:45:37.999383+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Nevada",
+          "country_code": "us",
+          "latitude": 38.8026097,
+          "longitude": -116.419389,
+          "mag": 69927,
+          "dead": 1338,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Connecticut",
+          "country_code": "us",
+          "latitude": 41.6032207,
+          "longitude": -73.087749,
+          "mag": 53108,
+          "dead": 4467,
+          "updated": "2020-09-03 18:45:37.999383+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Kansas",
+          "country_code": "us",
+          "latitude": 39.011902,
+          "longitude": -98.4842465,
+          "mag": 44529,
+          "dead": 458,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Washington",
+          "country_code": "us",
+          "latitude": 47.7510741,
+          "longitude": -120.7401385,
+          "mag": 78555,
+          "dead": 2026,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Pennsylvania",
+          "country_code": "us",
+          "latitude": 41.2033216,
+          "longitude": -77.1945247,
+          "mag": 140577,
+          "dead": 7792,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Maryland",
+          "country_code": "us",
+          "latitude": 39.0457549,
+          "longitude": -76.6412712,
+          "mag": 109808,
+          "dead": 3766,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Indiana",
+          "country_code": "us",
+          "latitude": 40.2671941,
+          "longitude": -86.1349019,
+          "mag": 98049,
+          "dead": 3325,
+          "updated": "2020-09-03 18:45:37.999383+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Oklahoma",
+          "country_code": "us",
+          "latitude": 35.0077519,
+          "longitude": -97.092877,
+          "mag": 60118,
+          "dead": 821,
+          "updated": "2020-09-03 18:45:37.999383+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "North Carolina",
+          "country_code": "us",
+          "latitude": 35.7595731,
+          "longitude": -79.0192997,
+          "mag": 171719,
+          "dead": 2779,
+          "updated": "2020-09-03 18:45:37.999383+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Delaware",
+          "country_code": "us",
+          "latitude": 38.9108325,
+          "longitude": -75.5276699,
+          "mag": 17549,
+          "dead": 606,
+          "updated": "2020-09-03 18:45:37.999383+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "North Dakota",
+          "country_code": "us",
+          "latitude": 47.5514926,
+          "longitude": -101.0020119,
+          "mag": 12271,
+          "dead": 152,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "District of Columbia",
+          "country_code": "us",
+          "latitude": 38.9071923,
+          "longitude": -77.0368707,
+          "mag": 14077,
+          "dead": 608,
+          "updated": "2020-09-03 18:45:37.999383+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Ohio",
+          "country_code": "us",
+          "latitude": 40.4172871,
+          "longitude": -82.907123,
+          "mag": 125768,
+          "dead": 4176,
+          "updated": "2020-09-03 18:45:37.999383+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Massachusetts",
+          "country_code": "us",
+          "latitude": 42.4072107,
+          "longitude": -71.3824374,
+          "mag": 129084,
+          "dead": 9059,
+          "updated": "2020-09-02 18:47:09.675065+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Idaho",
+          "country_code": "us",
+          "latitude": 44.0682019,
+          "longitude": -114.7420408,
+          "mag": 33059,
+          "dead": 374,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Louisiana",
+          "country_code": "us",
+          "latitude": 30.9842977,
+          "longitude": -91.9623327,
+          "mag": 150734,
+          "dead": 5004,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Michigan",
+          "country_code": "us",
+          "latitude": 44.3148443,
+          "longitude": -85.6023643,
+          "mag": 114603,
+          "dead": 6783,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Texas",
+          "country_code": "us",
+          "latitude": 31.9685988,
+          "longitude": -99.9018131,
+          "mag": 649844,
+          "dead": 13185,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Virginia",
+          "country_code": "us",
+          "latitude": 37.4315734,
+          "longitude": -78.6568942,
+          "mag": 122542,
+          "dead": 2641,
+          "updated": "2020-09-03 18:45:37.999383+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Alabama",
+          "country_code": "us",
+          "latitude": 32.3182314,
+          "longitude": -86.902298,
+          "mag": 128239,
+          "dead": 2217,
+          "updated": "2020-09-03 18:45:37.999383+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Arizona",
+          "country_code": "us",
+          "latitude": 34.0489281,
+          "longitude": -111.0937311,
+          "mag": 202898,
+          "dead": 5070,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Florida",
+          "country_code": "us",
+          "latitude": 27.6648274,
+          "longitude": -81.5157535,
+          "mag": 633442,
+          "dead": 11501,
+          "updated": "2020-09-03 18:45:37.999383+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Georgia",
+          "country_code": "us",
+          "latitude": 32.1656221,
+          "longitude": -82.9000751,
+          "mag": 274613,
+          "dead": 5661,
+          "updated": "2020-09-03 18:45:37.999383+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Hawaii",
+          "country_code": "us",
+          "latitude": 19.8967662,
+          "longitude": -155.5827818,
+          "mag": 8991,
+          "dead": 74,
+          "updated": "2020-09-03 18:45:37.999383+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "South Carolina",
+          "country_code": "us",
+          "latitude": 33.836081,
+          "longitude": -81.1637245,
+          "mag": 120503,
+          "dead": 2794,
+          "updated": "2020-09-03 18:45:37.999383+00:00"
+        },
+        "geometry": null
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "location": "Utah",
+          "country_code": "us",
+          "latitude": 39.3209801,
+          "longitude": -111.0937311,
+          "mag": 52950,
+          "dead": 414,
+          "updated": "2020-09-02 18:47:07.588724+00:00"
+        },
+        "geometry": null
+      }
+    ]
+    });
           "location": "New Hampshire",
           "country_code": "us",
           "latitude": 43.1938516,
